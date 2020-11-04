6def1be2ed1fe7882e4b7873a2ea642cca594fa0
Kevin Luo <kvn.luo@gmail.com>
2020-11-04 12:38:37 -0500

    created instance of mongodb online and connectd to db

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 // allows express to understand graphQL. provides simple way for express server to run the graphQL api. we do this by using it as middleware on a single route. this route will be like an endpoint to interact with our graphQL data
 const { graphqlHTTP } = require('express-graphql'); 
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected to mongodb atlas'))
+  .catch(err => console.log(`error: ${err}`))
 
 // schema used to map out the graph when someone makes a request to '/graphql'
 app.use('/graphql', graphqlHTTP({
